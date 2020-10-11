f5764ebc6cb16e4e0b99135ce3fbf4331b326bab
yasikahivin <38648741+yasikahivin@users.noreply.github.com>
2020-10-11 16:17:52 +0530

    distance sensing

--- Arduino/armServoFunctionComplete/armServoFunctionComplete.ino
+++ Arduino/armServoFunctionComplete/armServoFunctionComplete.ino
@@ -225,9 +225,28 @@ void testingState(){
   }
 
 void DistanceSensing(){
-  
+  long duration, inches, cm;
+   pinMode(pingPin, OUTPUT);
+   digitalWrite(pingPin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(pingPin, LOW);
+   pinMode(echoPin, INPUT);
+   duration = pulseIn(echoPin, HIGH);
+   cm = microsecondsToCentimeters(duration);
+   Serial.print(cm);
+   Serial.print("cm");
+   Serial.println();
+   delay(100);
+   
   }
 
+
+
+long microsecondsToCentimeters(long microseconds) {
+   return microseconds / 29 / 2;
+}
 //void servoIdentify(){
 //
 //  }
